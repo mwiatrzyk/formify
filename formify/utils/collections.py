f971092417a2f4e2640954fe6b0bffa6f72ceb95
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2013-04-28 20:21:36 +0200

    remove AttrDict class from collections

--- formify/utils/collections.py
+++ formify/utils/collections.py
@@ -1,50 +1,18 @@
-class AttrDict(dict):
-    """Dictionary with additional attribute-like access to keys.
-
-    Example:
-
-    >>> d = AttrDict({'foo': 1, 'bar': 2, 'baz': 3})
-    >>> d.foo
-    1
-    >>> d.bar
-    2
-    >>> d.baz
-    3
-    >>> d['foo'] == d.foo
-    True
-    >>> d['bar'] is d.bar
-    True
-    """
-
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
-
-    def __setattr__(self, name, value):
-        self[name] = value
-
-    def __delattr__(self, name):
-        try:
-            del self[name]
-        except KeyError:
-            raise AttributeError(name)
+"""General purpose collection classes."""
 
 
 class OrderedDict(object):
     """Dictionary that remembers order of its keys."""
 
     def __init__(self, iterable=None):
-        self._storage = {}
-        self._order = []
+        self.clear()
         for k, v in (iterable or []):
             self[k] = v
 
     def __setitem__(self, key, value):
-        self._storage[key] = value
-        if key not in self._order:
+        if key not in self._storage:
             self._order.append(key)
+        self._storage[key] = value
 
     def __getitem__(self, key):
         if key not in self._storage:
@@ -73,7 +41,7 @@ class OrderedDict(object):
 
     def clear(self):
         self._storage = {}
-        self._order = {}
+        self._order = []
 
     def copy(self):
         result = self.__class__()
