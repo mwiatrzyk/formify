e95763caead4672e0aa58c0d91d90bbf5bed2c35
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2013-05-12 09:09:41 +0200

    add functions to test if value is iterable, sequence or mapping

--- formify/utils/helpers.py
+++ formify/utils/helpers.py
@@ -17,3 +17,30 @@ def maybe_callable(value, *args, **kwargs):
         return value(*args, **kwargs)
     else:
         return value
+
+
+def is_iterable(value):
+    """Check if *value* is iterable.
+
+    An object is said to be an iterable object if it contains ``__iter__``
+    method.
+    """
+    return hasattr(value, '__iter__')
+
+
+def is_sequence(value):
+    """Check if *value* is a sequence.
+
+    An object is said to be a sequence if it contains ``__len__`` and
+    ``__getitem__`` methods.
+    """
+    return hasattr(value, '__len__') and hasattr(value, '__getitem__')
+
+
+def is_mapping(value):
+    """Check if *value* is a mapping.
+
+    An object is said to be a mapping if it is a sequence and contains ``keys``
+    method.
+    """
+    return is_sequence(value) and hasattr(value, 'keys')
